84ec8741cb54e99280b94657e1550cab563b5103
Martin <m87.wheeler@gmail.com>
2020-09-14 12:12:54 +0100

    Added 'Under Development' Modal
    When working on sites CosmicJS can be updated to show site may not be fully functional

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let windowDimensions = {
   height: window.innerHeight,
 }
 window.addEventListener(
-  "resize",
+  'resize',
   () => {
     windowDimensions = {
       width: window.innerWidth,
@@ -15,21 +15,21 @@ window.addEventListener(
 )
 
 // ***** open nav on hamburger click *****
-const HAMBURGER = document.querySelector("#hamburger")
-const MAIN_NAV = document.querySelector("#main-nav")
+const HAMBURGER = document.querySelector('#hamburger')
+const MAIN_NAV = document.querySelector('#main-nav')
 let navIsOpen = false
 
 const navOpenClose = () => {
   if (!navIsOpen) {
-    HAMBURGER.classList.replace("hamburger", "hamburger--open")
-    MAIN_NAV.classList.replace("main-nav", "main-nav--open")
+    HAMBURGER.classList.replace('hamburger', 'hamburger--open')
+    MAIN_NAV.classList.replace('main-nav', 'main-nav--open')
   } else {
-    HAMBURGER.classList.replace("hamburger--open", "hamburger")
-    MAIN_NAV.classList.replace("main-nav--open", "main-nav")
+    HAMBURGER.classList.replace('hamburger--open', 'hamburger')
+    MAIN_NAV.classList.replace('main-nav--open', 'main-nav')
   }
 }
 HAMBURGER.addEventListener(
-  "click",
+  'click',
   () => {
     navOpenClose()
     navIsOpen = !navIsOpen
@@ -38,13 +38,13 @@ HAMBURGER.addEventListener(
 )
 
 // ***** close nav when link selected *****
-const NAV_LINKS = document.querySelectorAll("#main-nav ul li")
+const NAV_LINKS = document.querySelectorAll('#main-nav ul li')
 NAV_LINKS.forEach(a => {
   a.addEventListener(
-    "click",
+    'click',
     () => {
-      HAMBURGER.classList.replace("hamburger--open", "hamburger")
-      MAIN_NAV.classList.replace("main-nav--open", "main-nav")
+      HAMBURGER.classList.replace('hamburger--open', 'hamburger')
+      MAIN_NAV.classList.replace('main-nav--open', 'main-nav')
       navIsOpen = false
     },
     false
@@ -62,22 +62,22 @@ const createClassElement = (type, className, text) => {
 
 // ***** create image element *****
 const createImg = (src, alt, className) => {
-  const image = document.createElement("img")
+  const image = document.createElement('img')
   className ? image.classList.add(className) : null
   image.src = src
-  image.setAttribute("alt", alt)
+  image.setAttribute('alt', alt)
 
   return image
 }
 
 // ***** create container for language icon *****
 const createLanguageContainer = (src, key) => {
-  const title = key.replace("lang_", "").replace("_", " ")
+  const title = key.replace('lang_', '').replace('_', ' ')
 
-  const container = createClassElement("div", "lang-container")
-  const img = createImg(src, title, "")
+  const container = createClassElement('div', 'lang-container')
+  const img = createImg(src, title, '')
   container.appendChild(img)
-  const project_title = createClassElement("p", "", title)
+  const project_title = createClassElement('p', '', title)
   container.appendChild(project_title)
 
   return container
@@ -85,10 +85,12 @@ const createLanguageContainer = (src, key) => {
 
 // ***** create a link to external site *****
 const createExternalButton = (text, className, url) => {
-  const button = createClassElement("a", className)
-  button.setAttribute("href", url)
-  button.setAttribute("target", "_blank")
-  button.setAttribute("rel", "noreferrer noopenner")
+  const button = createClassElement('a', className)
+  if (url) {
+    button.setAttribute('href', url)
+    button.setAttribute('target', '_blank')
+    button.setAttribute('rel', 'noreferrer noopenner')
+  }
   button.textContent = text
 
   return button
@@ -98,17 +100,18 @@ const createExternalButton = (text, className, url) => {
 const openProjectCard = e => {
   const cardClass = e.parentElement.classList.value
 
-  cardClass.includes("--open")
-    ? e.parentElement.classList.remove("project-card-wrapper--open")
-    : e.parentElement.classList.add("project-card-wrapper--open")
+  cardClass.includes('--open')
+    ? e.parentElement.classList.remove('project-card-wrapper--open')
+    : e.parentElement.classList.add('project-card-wrapper--open')
 }
 
 // ***** create project cards *****
 const PROJECT_CARDS_ARRAY = []
 const PROJECT_CARDS_CONTAINER = document.querySelector(
-  "#project-cards-container"
+  '#project-cards-container'
 )
 const createProjectCard = (
+  projectSlug,
   projectTitle,
   projectType,
   content,
@@ -117,47 +120,48 @@ const createProjectCard = (
   languages,
   index,
   project_url,
-  github_url
+  github_url,
+  underDevelopment
 ) => {
   // convert single digit numbers to double
   const doubleDigit = index + 1 < 10 ? `0${index + 1}` : `${index + 1}`
 
-  const wrapper = createClassElement("div", "project-card-wrapper")
+  const wrapper = createClassElement('div', 'project-card-wrapper')
   // wrapper.setAttribute("onclick", "openProjectCard(this); return false;")
-  const card = createClassElement("div", "project-card")
-  card.setAttribute("onclick", "openProjectCard(this); return false;")
+  const card = createClassElement('div', 'project-card')
+  card.setAttribute('onclick', 'openProjectCard(this); return false;')
   wrapper.appendChild(card)
-  const number = createClassElement("div", "project-number", doubleDigit)
+  const number = createClassElement('div', 'project-number', doubleDigit)
   card.appendChild(number)
-  const blank = createClassElement("div", "")
+  const blank = createClassElement('div', '')
   card.appendChild(blank)
-  const title = createClassElement("h1", "project-title", projectTitle)
+  const title = createClassElement('h1', 'project-title', projectTitle)
   card.appendChild(title)
-  const type = createClassElement("h3", "project-type", projectType)
+  const type = createClassElement('h3', 'project-type', projectType)
   card.appendChild(type)
-  const details = createClassElement("div", "project-details")
+  const details = createClassElement('div', 'project-details')
   wrapper.appendChild(details)
-  const project_logo = createImg(icon, "project logo", "project-logo")
+  const project_logo = createImg(icon, 'project logo', 'project-logo')
   details.appendChild(project_logo)
   const screenshot_wrapper = createClassElement(
-    "div",
-    "project-screenshot-wrapper"
+    'div',
+    'project-screenshot-wrapper'
   )
   details.appendChild(screenshot_wrapper)
   const project_screenshot = createImg(
     screenshot,
-    "project sreenshot",
-    "project-screenshot"
+    'project sreenshot',
+    'project-screenshot'
   )
   screenshot_wrapper.appendChild(project_screenshot)
-  const second_title = createClassElement("h3", "")
+  const second_title = createClassElement('h3', '')
   details.appendChild(second_title)
-  const project_text = createClassElement("div", "project-text")
+  const project_text = createClassElement('div', 'project-text')
   project_text.innerHTML = content
   details.appendChild(project_text)
-  const created_with = createClassElement("div", "created-with")
+  const created_with = createClassElement('div', 'created-with')
   details.appendChild(created_with)
-  const created_header = createClassElement("h3", "", "Created With")
+  const created_header = createClassElement('h3', '', 'Created With')
   created_with.appendChild(created_header)
 
   languages.forEach(lang => {
@@ -165,20 +169,81 @@ const createProjectCard = (
     created_with.appendChild(langEntry)
   })
 
-  const button_container = createClassElement("div", "buttons")
+  const button_container = createClassElement('div', 'buttons')
   details.appendChild(button_container)
-  const project_button = createExternalButton(
-    "Visit Site",
-    "button",
-    project_url
-  )
+  const project_button = underDevelopment
+    ? createExternalButton('Visit Site', 'button')
+    : createExternalButton('Visit Site', 'button', project_url)
   button_container.appendChild(project_button)
+
+  // github button
   const github_button = createExternalButton(
-    "View On Github",
-    "button--secondary",
+    'View On Github',
+    'button--secondary',
     github_url
   )
   button_container.appendChild(github_button)
+
+  // create modal to display when project is under development
+  let developmentModal = createClassElement('div', 'development-modal')
+  developmentModal.setAttribute('id', `modal-${projectSlug}`)
+
+  let modalParagraph_1 = createClassElement('p')
+  modalParagraph_1.textContent =
+    "I'm currently working on this site so some or all of the features may not work."
+  developmentModal.appendChild(modalParagraph_1)
+
+  let modalParagraph_2 = createClassElement('p')
+  modalParagraph_2.textContent =
+    'Do you want to try and visit anyway? You can always come back!'
+  developmentModal.appendChild(modalParagraph_2)
+
+  let remainButton = document.createElement('button')
+  remainButton.classList.add('button--danger')
+  remainButton.classList.add('button')
+  remainButton.textContent = 'NO'
+  remainButton.addEventListener(
+    'click',
+    () => {
+      developmentModal.style.visibility = 'hidden'
+      developmentModal.style.top = '-300vh'
+    },
+    false
+  )
+  developmentModal.appendChild(remainButton)
+
+  let continueButton = createExternalButton(
+    'YES',
+    'button--secondary',
+    project_url
+  )
+  continueButton.classList.add('button')
+  developmentModal.appendChild(continueButton)
+
+  project_button.addEventListener(
+    'click',
+    () => {
+      if (underDevelopment) {
+        let modal = document.querySelector(`#modal-${projectSlug}`)
+        console.log(modal)
+        modal.style.visibility = 'visible'
+        modal.style.top = '0.5rem'
+      }
+    },
+    false
+  )
+  details.appendChild(developmentModal)
+  details.appendChild(developmentModal)
+
+  // display banner if under development
+  if (underDevelopment) {
+    let dev_banner = createClassElement(
+      'div',
+      'under-development-banner',
+      'Under Development'
+    )
+    details.appendChild(dev_banner)
+  }
 
   PROJECT_CARDS_ARRAY.push(wrapper)
   PROJECT_CARDS_CONTAINER.appendChild(wrapper)
@@ -186,7 +251,7 @@ const createProjectCard = (
 
 // ***** fetch Project Cards data from CosmicJS *****
 let projectDataLoading = true
-const LOADING_SPINNER = document.querySelector("#spinner-container")
+const LOADING_SPINNER = document.querySelector('#spinner-container')
 
 const projectData = `https://api.cosmicjs.com/v1/mwwdd-blog/objects?pretty=true&hide_metafields=true&type=projects&read_key=N6C2ydBXJRnJGr5xKPQfW16ea2qANsnZoNgLzW5hXvAUIjN8FY&limit=20&props=slug,title,content,metadata,`
 const fetchProjectData = async url => {
@@ -195,10 +260,11 @@ const fetchProjectData = async url => {
     const data = await res.json()
     await data.objects.forEach((object, i) => {
       const objectEntries = Object.entries(object.metadata)
-        .filter(entry => entry[0].substr(0, 5) === "lang_")
+        .filter(entry => entry[0].substr(0, 5) === 'lang_')
         .filter(entry => entry[1].url !== null)
 
       createProjectCard(
+        object.slug,
         object.title,
         object.metadata.project_type,
         object.content,
@@ -207,14 +273,15 @@ const fetchProjectData = async url => {
         objectEntries,
         i,
         object.metadata.project_url,
-        object.metadata.github_url
+        object.metadata.github_url,
+        object.metadata.under_development
       )
 
-      LOADING_SPINNER.style.display = "none"
+      LOADING_SPINNER.style.display = 'none'
       projectDataLoading = false
     })
   } catch (err) {
-    console.log("Project Data Error", err)
+    console.log('Project Data Error', err)
   }
 }
 fetchProjectData(projectData)
@@ -236,21 +303,21 @@ const isLoading = setInterval(() => {
     arrangeProjectCards()
 
     // ***** flip cards on scroll *****
-    document.querySelector("#project-cards-container").addEventListener(
-      "scroll",
+    document.querySelector('#project-cards-container').addEventListener(
+      'scroll',
       () => {
         PROJECT_CARDS_ARRAY.forEach((card, i) => {
           if (card.getBoundingClientRect().x <= windowDimensions.width * 0.45) {
-            card.style.transform = "rotateY(0)"
+            card.style.transform = 'rotateY(0)'
           } else {
-            card.style.transform = "rotateY(-50deg)"
+            card.style.transform = 'rotateY(-50deg)'
           }
         })
       },
       false
     )
   }
-  PROJECT_CARDS_ARRAY[0].style.transform = "rotateY(0)"
+  PROJECT_CARDS_ARRAY[0].style.transform = 'rotateY(0)'
 }, 500)
 
 // // ***** rotate cards when in view *****
--- splashpage.js
+++ splashpage.js
@@ -1,32 +1,32 @@
-const enableSplash = true // for production
+const enableSplash = false // for production
 
-const SPLASH_PAGE = document.querySelector("#splash-page")
+const SPLASH_PAGE = document.querySelector('#splash-page')
 const ANIMATION_WRAPPER = document.querySelector(
-  "#animated-logo-wrapper .animation-wrapper"
+  '#animated-logo-wrapper .animation-wrapper'
 )
-const ELEMS_TOP_BLOCK_FALL = document.querySelectorAll(".anim-top-block-fall")
+const ELEMS_TOP_BLOCK_FALL = document.querySelectorAll('.anim-top-block-fall')
 const ELEMS_BOTTOM_BLOCK_FALL = document.querySelectorAll(
-  ".anim-bottom-block-fall"
+  '.anim-bottom-block-fall'
 )
 const LEFT_DIAMOND = document.querySelector(
-  "#animated-logo-wrapper .left-diamond"
+  '#animated-logo-wrapper .left-diamond'
 )
 const RIGHT_DIAMOND = document.querySelector(
-  "#animated-logo-wrapper .right-diamond"
+  '#animated-logo-wrapper .right-diamond'
 )
-const SPLASH_PAGE_MAIN_LOGO = document.querySelector("#splash-page .main-logo")
+const SPLASH_PAGE_MAIN_LOGO = document.querySelector('#splash-page .main-logo')
 
 const animateOnLoad = () => {
-  ANIMATION_WRAPPER.style.animationName = "rotate-logo"
+  ANIMATION_WRAPPER.style.animationName = 'rotate-logo'
   ELEMS_TOP_BLOCK_FALL.forEach(
-    elem => (elem.style.animationName = "top-block-fall")
+    elem => (elem.style.animationName = 'top-block-fall')
   )
   ELEMS_BOTTOM_BLOCK_FALL.forEach(
-    elem => (elem.style.animationName = "bottom-block-fall")
+    elem => (elem.style.animationName = 'bottom-block-fall')
   )
-  LEFT_DIAMOND.style.animationName = "left-diamond-slide"
-  RIGHT_DIAMOND.style.animationName = "right-diamond-slide"
-  SPLASH_PAGE_MAIN_LOGO.style.animationName = "slide-up-and-color"
+  LEFT_DIAMOND.style.animationName = 'left-diamond-slide'
+  RIGHT_DIAMOND.style.animationName = 'right-diamond-slide'
+  SPLASH_PAGE_MAIN_LOGO.style.animationName = 'slide-up-and-color'
 }
 
 // ***** determine splash page display on load *****
@@ -34,32 +34,32 @@ const animateOnLoad = () => {
 const playSplashPage = bool => {
   if (bool) {
     // disable scroll on body and add padding on desktop
-    document.body.style.overflowY = "hidden"
+    document.body.style.overflowY = 'hidden'
     window.innerWidth >= 1280
-      ? (document.body.style.padding = "0 15px 0 0")
+      ? (document.body.style.padding = '0 15px 0 0')
       : null
     // show splash page
-    SPLASH_PAGE.style.display = "block"
+    SPLASH_PAGE.style.display = 'block'
     // generate animations
     animateOnLoad()
     // hide page after set time determined by CSS animations
     setTimeout(() => {
-      SPLASH_PAGE.style.opacity = "0"
+      SPLASH_PAGE.style.opacity = '0'
       setTimeout(() => {
-        SPLASH_PAGE.style.display = "none"
-        document.body.style.overflowY = "scroll"
-        window.innerWidth >= 1280 ? (document.body.style.padding = "0") : null
+        SPLASH_PAGE.style.display = 'none'
+        document.body.style.overflowY = 'scroll'
+        window.innerWidth >= 1280 ? (document.body.style.padding = '0') : null
       }, 500)
     }, 9000)
   } else {
-    SPLASH_PAGE.style.display = "none"
+    SPLASH_PAGE.style.display = 'none'
   }
 }
 
 // ***** session storage *****
-if (!sessionStorage.getItem("visited")) {
+if (!sessionStorage.getItem('visited')) {
   playSplashPage(enableSplash)
-  sessionStorage.setItem("visited", true)
+  sessionStorage.setItem('visited', true)
 } else {
-  SPLASH_PAGE.style.display = "none"
+  SPLASH_PAGE.style.display = 'none'
 }
