760150d2f0434230e8efdb23ff071bdbfd855732
Martin <m87.wheeler@gmail.com>
2020-09-14 13:08:54 +0100

    Update splashpage.js

--- splashpage.js
+++ splashpage.js
@@ -1,4 +1,4 @@
-const enableSplash = false // for production
+const enableSplash = true // for production
 
 const SPLASH_PAGE = document.querySelector('#splash-page')
 const ANIMATION_WRAPPER = document.querySelector(
